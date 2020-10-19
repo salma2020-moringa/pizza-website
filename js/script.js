6a67c9b21266d31903482f9cd793c70eddc5116c
salma2020-moringa <salmaladha5@gmail.com>
2020-10-19 12:20:36 +0200

    commit changes of script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ $document.ready(function(){
 
 }
 //owl banner-slide
-$("#banner-slide.Owl-Carousel"){
-
+$("#banner-slide.Owl-Carousel").owlCorousel({
+    items:1,
+    loop:true,
+    autoplay:true,
 }
