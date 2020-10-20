4e7c8c87ed3dcaea2a56d4518fcc16669179ac80
salma2020-moringa <salmaladha5@gmail.com>
2020-10-20 23:54:34 +0200

    slider and uploader addition

--- js/script.js
+++ js/script.js
@@ -8,3 +8,87 @@ $(document).ready(function() {
         autoplay: true
     });
 });
+//preloader
+$(window).on("load", function () {
+    $(".status").fadeOut();
+    $("#preloader").delay(500).fadeOut();
+  });
+  (".slider").slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    loop: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+  $(".slider-review").slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    loop: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+        },
+      },
